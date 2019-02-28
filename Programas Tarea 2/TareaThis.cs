bd7ddc075d5b7a94473e0e023fb4371598dec7b6
jr110 <47792327+jr110@users.noreply.github.com>
2019-02-27 20:14:40 -0800

    Add files via upload

---
+++ Programas Tarea 2/TareaThis.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TareasThis
+{
+    class Program
+    {
+        class persona
+        {
+            public string Nombre,Apellido;
+            public persona(string N, string A)
+            {
+                this.Nombre = N;
+                this.Apellido=A;
+            }
+
+            public void imprimirpersona()
+        {
+            Console.WriteLine("{0} {1}",Nombre,Apellido);
+        }
+        }
+
+        
+        static void Main(string[] args)
+        {
+            persona Pedro= new persona("pedro","perez");
+            Pedro.imprimirpersona();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
